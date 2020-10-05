0f3d52fbbac3ae883dfe93cad7de798149368d4e
Oliver <oli.richards@gmail.com>
2020-10-05 14:12:45 +1100

    Added database scheme. Graphene_mongo is not liking my lists of embedded documents so they are removed for now

--- backend/webserver/schema.py
+++ backend/webserver/schema.py
@@ -1,35 +1,38 @@
 import graphene
 from graphene.relay import Node
 from graphene_mongo import MongoengineConnectionField, MongoengineObjectType
-from models import Department as DepartmentModel
-from models import Employee as EmployeeModel
-from models import Role as RoleModel
+import models
 
-class Department(MongoengineObjectType):
-
+class PodcastEpisode(MongoengineObjectType):
     class Meta:
-        model = DepartmentModel
+        model = models.PodcastEpisode
         interfaces = (Node,)
 
-
-class Role(MongoengineObjectType):
-
+class PodcastMetadata(MongoengineObjectType):
     class Meta:
-        model = RoleModel
+        model = models.PodcastMetadata
         interfaces = (Node,)
 
-
-class Employee(MongoengineObjectType):
-
+class User(MongoengineObjectType):
     class Meta:
-        model = EmployeeModel
+        model = models.User
         interfaces = (Node,)
+
+class PodcastEpisodeMetadata(MongoengineObjectType):
+    class Meta:
+        model = models.PodcastEpisodeMetadata
+        interface = (Node,)
+
+class ListenHistoryEntry(MongoengineObjectType):
+    class Meta:
+        model = models.ListenHistoryEntry
+        interface = (Node,)
 
 
 class Query(graphene.ObjectType):
     node = Node.Field()
-    all_employees = MongoengineConnectionField(Employee)
-    all_role = MongoengineConnectionField(Role)
-    role = graphene.Field(Role)
+    all_podcast_episode = MongoengineConnectionField(PodcastEpisode)
+    all_podcast_metadata = MongoengineConnectionField(PodcastMetadata)
+    all_user = MongoengineConnectionField(User)
 
-schema = graphene.Schema(query=Query, types=[Department, Employee, Role])
+schema = graphene.Schema(query=Query, types=[PodcastEpisode, PodcastMetadata, PodcastEpisodeMetadata, User])
