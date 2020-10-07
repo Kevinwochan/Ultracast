dec8598b18de1975604b5f6f4b1e1b581bb7feb3
Daniel Latimer <latdani@gmail.com>
2020-10-07 13:52:06 +1100

    Moved branding to frontend & updated theme

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -3,6 +3,32 @@ import { createMuiTheme } from "@material-ui/core/styles";
 
 // A custom theme for this app
 const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      "Oxygen",
+      "Ubuntu",
+      "Cantarell",
+      "Fira Sans",
+      "Droid Sans",
+      "Helvetica Neue",
+      "sans-serif",
+    ].join(","),
+  },
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        body: {
+          margin: 0,
+          "-webkit-font-smoothing": "antialiased",
+          "-moz-osx-font-smoothing": "grayscale",
+        },
+      },
+    },
+  },
   palette: {
     primary: {
       main: "#102B32",
