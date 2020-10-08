780a5c0df621580067ca2fe236ea0934ed047b67
kevinwochan <kevinwochan@gmail.com>
2020-10-08 19:24:18 +1100

    enabled debug mode

--- backend/webserver/app.py
+++ backend/webserver/app.py
@@ -10,11 +10,10 @@ from graphene_file_upload.flask import FileUploadGraphQLView
 app = Flask(__name__)
 app.debug = True
 
-CORS(app, resources={r"/graphql/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 # Graphql rules
-
 app.add_url_rule(
     '/graphql',
     view_func=FileUploadGraphQLView.as_view('graphql', schema=schema, graphiql=True)
@@ -23,4 +22,4 @@ app.add_url_rule(
 
 if __name__ == '__main__':
     init_db()
-    app.run()
+    app.run(debug=True)
