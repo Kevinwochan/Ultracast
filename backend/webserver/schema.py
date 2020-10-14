be3366a14b3a7046aa086c0231abd8f5c470344a
Oliver <oli.richards@gmail.com>
2020-10-14 22:06:03 +1100

    Added function to lookup relay ID of query type

--- backend/webserver/schema.py
+++ backend/webserver/schema.py
@@ -15,6 +15,17 @@ import graphene_file_upload.scalars
 schema = graphene.Schema(query=query.Query, mutation=mutations.Mutations, 
         types=query.types)
 
+def get_relay_id(query_class, mongodb_id):
+    '''
+    Technically the query class should be the class that implements graphene.relay.Node
+    Since the to_global_id just uses the class name and 
+    we've matched up the names of our classes in models.py and query.py this doesnt really matter
+    But you've been warned future me
+    Example usage: get_relay_id(query.User, "123")
+    '''
+    assert(graphene.relay.is_node(query_class))
+    return graphene.relay.Node.to_global_id(query_class.__name__, mongodb_id)
+
 def saveSchema(path: str):
     with open(path, "w") as fp:
         schema_str = graphql.utils.schema_printer.print_schema(schema)
