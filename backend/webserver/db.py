f88019ec5053af10f66f8fc98633ecafccca38a0
C-O-Shea <oshconn@gmail.com>
2020-10-17 22:45:06 +1100

    Added db methods to add/delete static content store on OD space

--- backend/webserver/db.py
+++ backend/webserver/db.py
@@ -1,34 +1,48 @@
 from . import models
 from . import schema
 
+import boto3
+from botocore.client import Config
 from mongoengine import connect
 
-# Amazon EC2 instance
+# Mongodb connection
 MONGO_USERNAME = 'ultracast_admin'
 MONGO_PASSWORD = 'vtcXHq7fS$si9$Bi6c&2'
 MONGO_IP = '139.59.227.230'
 MONGO_AUTH_DB = 'admin'
 MONGO_DB = 'ultracast_sandbox'
-'''
-# Local mongo instance
-MONGO_USERNAME = 'ultracast'
-MONGO_PASSWORD = 'abcdefg'
-MONGO_IP = 'localhost'
-MONGO_AUTH_DB = 'admin'
-'''
-
-'''
-To setup a local user, open up mongo terminal and then execute this query:
-use admin
-db.createUser({
-    user: "ultracast", 
-    pwd: "abcdefg",
-    roles: [{role: "readWrite", db: "ultracast_sandbox"}]
-    })
-'''
 
 MONGO_URI = f'mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_IP}/{MONGO_DB}?authSource={MONGO_AUTH_DB}'
 
 connect(host=MONGO_URI)
 
+# Digital Ocean Space (Static-Files)
+session = boto3.session.Session()
+client = session.client('s3',
+                        region_name='sfo2',
+                        endpoint_url='https://sfo2.digitaloceanspaces.com',
+                        aws_access_key_id='CUT4SK6OYILHJJV3B5LD',
+                        aws_secret_access_key='yyIXed9h9kn6n9V4c/b64+ZRHtP8baR89lp3dqvOY34')
 
+BUCKET = 'ultracast-files'
+FILE_ACCESS = 'public-read'
+
+def checkStatus(resp, ok_statuses):
+    if resp['ResponseMetadata']['HTTPStatusCode'] not in ok_statuses:
+        print(resp)
+        return False
+    return True
+
+def addFile(filename, file):
+    resp = client.put_object(Body=file, Bucket=BUCKET, Key=filename, ACL=FILE_ACCESS)
+    return checkStatus(resp, [200])
+
+def removeFile(filename):
+    resp = client.delete_object(Bucket=BUCKET, Key=filename)
+    return checkStatus(resp, [200, 204])
+
+if __name__ == '__main__':
+    in_file = open("./webserver/resources/ff-16b-2c-44100hz.mp4", "rb")
+    data = in_file.read()
+    addAudioFile("test-file", data)
+    # removeAudioFile("test-file")
