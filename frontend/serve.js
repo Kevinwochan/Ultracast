78dc69ee5da9b94f0dd615a904384d1581fdf451
kevinwochan <kevinwochan@gmail.com>
2020-11-05 02:26:29 +1100

    added deployment script to frontend

---
+++ frontend/serve.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+port = process.argv[2] || 4000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port);
+
+console.log(`Serving on port ${port}`);
