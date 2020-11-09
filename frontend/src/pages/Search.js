120693fe6938a98e3d5ac30c2adbbe58be851074
Daniel Latimer <latdani@gmail.com>
2020-11-10 03:01:17 +1100

    Started on saving search as stream

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
 import Box from "@material-ui/core/Box";
 import algoliasearch from "algoliasearch/lite";
 import {
@@ -47,8 +48,17 @@ const Autocomplete = () => (
     <Box mb={2}>
       <SearchBox />
     </Box>
-    <Box mb={2}>
+    <Box
+      mb={2}
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
       <PoweredBy />
+      <Button variant="contained" color="primary">
+        Save search as stream
+      </Button>
     </Box>
     <Results>
       <Hits hitComponent={Hit} />
