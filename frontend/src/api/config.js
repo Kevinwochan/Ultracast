0fe6b3e9caf03a906afc6aa12e529436fff6d398
C-O-Shea <oshconn@gmail.com>
2020-11-14 00:47:28 +1100

    Frontend connect to remote backend

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,5 +1,5 @@
 import algoliasearch from "algoliasearch/lite";
 export default {
-  BACKEND_ENDPOINT: "http://localhost:5000/graphql",
+  BACKEND_ENDPOINT: "http://139.59.227.230:5000/graphql",
   ALGOLIA_CLIENT: algoliasearch("DLUH4B7HCZ", "85303ce5c9827fc85ed49f641c6963a6"),
 };
