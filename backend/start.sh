7fa694611cad9a1f149820aae0fd753cb6419f01
server <anon@example.com>
2020-11-14 06:15:48 +0000

    Only set environment vars if they are not already set

--- backend/start.sh
+++ backend/start.sh
@@ -18,5 +18,7 @@ if [ -n "$(diff <( pip freeze) <( cat requirements.txt))" ]; then
     pip install -r requirements.txt
 fi
 
-export ULTRACAST_BACKEND_SETTINGS="$(dirname $(realpath $0))/config/development_settings.py"
+if [[ -z "${ULTRACAST_BACKEND_SETTINGS}" ]]; then
+    export ULTRACAST_BACKEND_SETTINGS="$(dirname $(realpath $0))/config/development_settings.py"
+fi
 python backend.py
