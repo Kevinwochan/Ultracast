0864068c5cb691999bfbc4882ba98da25a96d985
kevinwochan <kevinwochan@gmail.com>
2020-11-15 17:53:12 +1100

    using OS assigned port if none is provided

--- frontend/serve.js
+++ frontend/serve.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-port = process.argv[2] || 4000;
+port = process.argv[2] || 0;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
