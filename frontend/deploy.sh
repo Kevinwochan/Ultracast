e01d17ffacbebfb382725f8b96db655a878b5b6d
kevinwochan <kevinwochan@gmail.com>
2020-11-15 13:16:38 +1100

    added --local flag for using a local backend endpoint

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
 npm install --production
-npm run build
+
+if [ $1 = "--local" ]
+then
+	npm run local
+
+else
+	npm run build
+fi
+
 node serve.js
--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,5 +1,5 @@
 import algoliasearch from "algoliasearch/lite";
 export default {
-  BACKEND_ENDPOINT: "http://139.59.227.230:5000/graphql",
+  BACKEND_ENDPOINT: process.env.NODE_ENV ==="local" ? "http://139.59.227.230:5000/graphql" : "http://localhost:5000/graphql"  ,
   ALGOLIA_CLIENT: algoliasearch("DLUH4B7HCZ", "85303ce5c9827fc85ed49f641c6963a6"),
 };
